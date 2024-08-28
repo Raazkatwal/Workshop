ecd670727884c1a3b336f7489ddaa10518627bf9
Raaz <raazkatwal5@gmail.com>
2024-08-28 12:35:56 +0545

    added new views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_question_list = Question.objects.order_by('pub_date')
+    output = "<br><br>".join(q.question_text for q in latest_question_list)
+    return HttpResponse(output)
+
+
+def view(request, question_id):
+    response = "You are looking at question %s"
+    return HttpResponse(response %question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at the results of question id %s"
+    return HttpResponse(response %question_id)
+
+
+def vote(request, question_id):
+    response = "You are voting on question %s"
+    return HttpResponse(response %question_id)
