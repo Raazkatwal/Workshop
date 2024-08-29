fd56166d14d69e1e4bb6df54c053e8a37134bc1c
Raaz <raazkatwal5@gmail.com>
2024-08-29 12:44:35 +0545

    added html for view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,22 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')
-    output = "<br><br>".join(q.question_text for q in latest_question_list)
-    return HttpResponse(output)
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
 
 
 def view(request, question_id):
-    response = "You are looking at question %s"
-    return HttpResponse(response %question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
